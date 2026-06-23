54951449e344965b404af1e735b0866e26da12c1
priyakutty4740-cpu <priyakutty4740@gmail.com>
2026-06-23 20:48:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-// Welcome Button
 function showMessage() {
-    alert("🎉 Welcome to EduSmart! Happy Learning.");
+    window.location.href = "courses.html";
 }
 
-// Contact Form
-const form = document.querySelector("form");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const name = document.querySelector('input[type="text"]').value;
-    const email = document.querySelector('input[type="email"]').value;
-
-    if (name === "" || email === "") {
-        alert("Please fill all the required fields.");
-    } else {
-        alert("✅ Thank you, " + name + "! Your message has been sent successfully.");
-        form.reset();
+    if (form) {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            alert("Thank you! Your message has been sent successfully.");
+            form.reset();
+        });
     }
 });
